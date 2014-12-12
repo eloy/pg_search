ef437ddd490cf1f926aa4b728f36ea5b3356f4cb
Grant Hutchins <pair+ghutchins@pivotal.io>
2014-12-12 16:31:02 -0600

    Rubocop: Address Style/BarePercentLiterals

--- spec/lib/pg_search/multisearch/rebuilder_spec.rb
+++ spec/lib/pg_search/multisearch/rebuilder_spec.rb
@@ -114,9 +114,15 @@ describe PgSearch::Multisearch::Rebuilder do
 
             executed_sql = []
 
+<<<<<<< HEAD
             notifier = ActiveSupport::Notifications.subscribe("sql.active_record") do |_name, _start, _finish, _id, payload|
               executed_sql << payload[:sql] if payload[:sql].include?(%Q{INSERT INTO "pg_search_documents"})
             end
+=======
+          notifier = ActiveSupport::Notifications.subscribe("sql.active_record") do |_name, _start, _finish, _id, payload|
+            executed_sql << payload[:sql] if payload[:sql].include?(%{INSERT INTO "pg_search_documents"})
+          end
+>>>>>>> Rubocop: Address Style/BarePercentLiterals
 
             rebuilder.rebuild
             ActiveSupport::Notifications.unsubscribe(notifier)
